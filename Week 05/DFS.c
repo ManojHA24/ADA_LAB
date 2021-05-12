d43b8442f436b24f749392be1d987f152a91a530
ManojHA24 <71307033+ManojHA24@users.noreply.github.com>
2021-05-12 14:11:57 +0530

    Create DFS.c

---
+++ Week 05/DFS.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void dfs(int);
+
+int a[10][10],vis[10],n,count;
+
+void main()
+
+{
+
+int i,j,src;
+printf("enter number of vertices\n");
+scanf("%d",&n);
+printf("enter adjacency matrix\n");
+for(i=1;i<=n;i++)
+{
+for(j=1;j<=n;j++)
+{
+scanf("%d", &a[i][j]);
+}
+}
+
+for(i=1;i<=n;i++)
+ vis[i]=0;
+printf("enter source vertex\n");
+scanf("%d",&src);
+printf("Nodes reachable from vertex-%d \n",src);
+dfs(src);
+if(count==n){
+    printf("\nGraph is connected\n");
+}
+else{
+    printf("Graph is not connected");
+}
+}
+
+void dfs(int v)
+{
+
+int i;
+vis[v]=1;
+count=count+1;
+printf("%d,",v);
+for(i=1;i<=n;i++)
+{
+if(a[v][i]==1 && vis[i]==0)
+  dfs(i);
+}
+}
