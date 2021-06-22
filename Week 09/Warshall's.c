71fe0b4533d9baf4f554d7776eb5b11962040159
ManojHA24 <71307033+ManojHA24@users.noreply.github.com>
2021-06-22 15:32:06 +0530

    Create Warshall's.c

---
+++ Week 09/Warshall's.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+
+void warshalls();
+
+int a[10][10], p[10][10], i,j,k,n;
+
+void main()
+{
+printf("Enter number of vertices\n");
+scanf("%d",&n);
+printf("enter adjacency matrix\n");
+for(i=1;i<=n;i++)
+  {
+    for(j=1;j<=n;j++)
+   {
+    scanf("%d",&a[i][j]);
+  }
+}
+warshalls();
+printf("Path Matrix\n");
+for(i=1;i<=n;i++)
+   {
+    for(j=1;j<=n;j++)
+    {
+      printf("%d\t",p[i][j]);
+     }
+     printf("\n");
+  }
+getch();
+}
+void warshalls()
+{
+for(i=1;i<=n;i++)
+  {
+   for(j=1;j<=n;j++)
+   {
+    p[i][j]=a[i][j];
+   }
+}
+    for(k=1;k<=n;k++)
+    {
+      for(i=1;i<=n;i++)
+       {
+         for(j=1;j<=n;j++)
+           {
+              if((p[i][j]!=1) && (p[i][k]==1 && p[k][j]==1))
+             p[i][j]=1;
+         }
+      }
+   }
+}
